295a24aa95527f0fa9bd10ba253dcc919ef7f3a1
marcos.florencio <marcos.florencio@tecnospeed.com.br>
2019-06-06 19:13:27 -0300

    feat: add text search

--- test/lib/filter.test.js
+++ test/lib/filter.test.js
@@ -4,8 +4,50 @@ const users = require('../assets/data.json')
 
 const { filter } = require('../../lib')
 
-test('should return the websites', t => {
+test('getWebsites // should return the websites', t => {
   const expected = ['hildegard.org', 'anastasia.net', 'ramiro.info']
   const result = filter.getWebsites({ users })
   t.deepEqual(expected, result)
+})
+
+test('containsTextInProperties // should search in a object', t => {
+  t.true(filter.containsTextInProperties({
+    properties: ['street', 'suite', 'city'],
+    data: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough'
+    },
+    search: 'light'
+  }))
+
+  t.true(filter.containsTextInProperties({
+    properties: ['street', 'suite', 'city'],
+    data: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough'
+    },
+    search: 'apt'
+  }))
+
+  t.true(filter.containsTextInProperties({
+    properties: ['street', 'suite', 'city'],
+    data: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough'
+    },
+    search: 'gwenBoroUgh'
+  }))
+
+  t.false(filter.containsTextInProperties({
+    properties: ['street', 'suite', 'city'],
+    data: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough'
+    },
+    search: 'Batata'
+  }))
 })
