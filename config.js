778b4cad737dfe573e98317fba1b686d3cae0afd
eptaccio <eptaccio@gmail.com>
2019-06-10 01:59:28 -0300

    feat: add elastic search

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const { env } = process
 
 const config = {
   APP_PORT: env.APP_PORT || 3000,
-  DATA_URL: 'https://jsonplaceholder.typicode.com/users'
+  DATA_URL: 'https://jsonplaceholder.typicode.com/users',
+  ELASTIC_SEARCH_URL: env.ELASTIC_SEARCH_URL || 'http://localhost:9200'
 }
 
 module.exports = {
